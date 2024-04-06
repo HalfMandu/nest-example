293239c8ca91effe59d1f2580cc3771b0e273ac4
Stephen Rinkus <jasperwednesday@gmail.com>
2024-04-06 13:56:02 -0700

    Scaffolding new NestJs example app...

---
+++ notes.js
@@ -0,0 +1,34 @@
+
+Basic NestJs App 
+////////////////////////////////////////////////////////////////////////
+
+
+
+npx @nestjs/cli new nest-example
+cd nest-example
+nest generate controller example
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
