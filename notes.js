f5421f4643f8d1f0f10758aa23239d61d8fc6370
Stephen Rinkus <jasperwednesday@gmail.com>
2024-04-06 14:10:59 -0700

    creating example controller

--- notes.js
+++ notes.js
@@ -5,8 +5,12 @@ Basic NestJs App
 
 
 npx @nestjs/cli new nest-example
+
 cd nest-example
-nest generate controller example
+
+nest generate controller example		
+
+	This command will create a new controller file named “example.controller.ts” along with a corresponding unit test file.
 
 
 
