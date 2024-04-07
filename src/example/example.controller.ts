d403df7f363052bf3e2ba596a75a8756a5d154aa
Stephen Rinkus <jasperwednesday@gmail.com>
2024-04-07 12:05:52 -0700

    adding service and basic working injection for GET

--- src/example/example.controller.ts
+++ src/example/example.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ExampleService } from './example.service';
 
 //Defining a simple GET route to return a JSON response
 @Controller('example')
 export class ExampleController {
+  constructor(private readonly exampleService: ExampleService) {}
+
   @Get()
   getExample(): string {
-    return 'Hello, NestJS!';
+    return this.exampleService.getHello();
   }
 }
