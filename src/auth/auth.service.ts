3954409c9dbb1d7f9b70efaeb361ddaba37c0b64
Stephen Rinkus <jasperwednesday@gmail.com>
2024-04-07 21:56:56 -0700

    importing JWT into auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,10 +13,14 @@ export class AuthService {
     username: string,
     pass: string,
   ): Promise<{ access_token: string }> {
+    console.log('Signing in, checking username/password...');
     const user = await this.usersService.findOne(username);
+    console.log(user.password);
+    console.log(pass);
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
+    console.log('User entered PW matches stored PW, granting token...');
     const payload = { sub: user.userId, username: user.username };
     return {
       access_token: await this.jwtService.signAsync(payload),
